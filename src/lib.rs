6339722dda30b57301d8d303440a3602a1bb67de
Marc Brinkmann <git@marcbrinkmann.de>
2016-03-12 01:33:51 +0100

    Added initial comments.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Software PID controller
+//!
+//! Owes a great debt to:
+//!
+//! * http://www.embedded.com/design/prototyping-and-development/4211211/PID-without-a-PhD
+//! * http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
