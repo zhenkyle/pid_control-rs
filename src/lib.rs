d1ccf5dba8e768e2bbe07978facf57cd162c7dbe
Marc Brinkmann <git@marcbrinkmann.de>
2016-03-12 11:51:20 +0100

    New implementation, closer to formula in Wikipedia.

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,13 @@
 //!
 //! Owes a great debt to:
 //!
+//! * https://en.wikipedia.org/wiki/PID_controller
 //! * http://www.embedded.com/design/prototyping-and-development/4211211/PID-without-a-PhD
 //! * http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
 
 extern crate core;
 
 use std::f64;
-
-pub struct PIDController {
-    /// Proportional gain
-    pub p_gain: f64,
-
-    /// Integral gain
-    pub i_gain: f64,
-
-    /// Differential gain,
-    pub d_gain: f64,
-
-    /// Collected i
-    pub i_state: f64,
-
-    /// Previous input value
-    pub prev_value: f64,
-
-    /// Output range limits
-    pub i_min: f64,
-    pub i_max: f64,
-}
 
 #[inline]
 fn limit_range<T>(min: T, max: T, value: T) -> T
@@ -43,51 +23,64 @@ where T: PartialOrd {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct PIDController {
+    /// Proportional gain
+    pub p_gain: f64,
+
+    /// Integral gain
+    pub i_gain: f64,
+
+    /// Differential gain,
+    pub d_gain: f64,
+
+    pub target: f64,
+
+    /// Collected i
+    pub err_sum: f64,
+
+    /// Previous input value
+    pub prev_error: f64,
+
+    /// Output range limits
+    pub i_min: f64,
+    pub i_max: f64,
+}
+
 impl PIDController {
     pub fn new(p_gain: f64, i_gain: f64, d_gain: f64) -> PIDController {
         PIDController{
             p_gain: p_gain,
             i_gain: i_gain,
             d_gain: d_gain,
-            i_state: 0.0,
-            prev_value: 0.0,
+
+            target: 0.0,
+
+            err_sum: 0.0,
+            prev_error: 0.0,
+
             i_min: -f64::INFINITY,
             i_max: f64::INFINITY,
         }
     }
 
-    pub fn update(&mut self, error: f64, value: f64) -> f64 {
+    pub fn update(&mut self, value: f64, delta_t: f64) -> f64 {
+        let error = self.target - value;
+
         // PROPORTIONAL
         let p_term = self.p_gain * error;
 
         // INTEGRAL
-        self.i_state += error;
-        let i_term = limit_range(self.i_min, self.i_max,
-                                 self.i_gain * self.i_state);
+        self.err_sum += limit_range(
+            self.i_min, self.i_max,
+            self.err_sum + error * delta_t
+        );
+        let i_term = self.i_gain * self.err_sum;
 
         // DIFFERENTIAL
-        let d_term = self.d_gain * (self.prev_value - value);
-        self.prev_value = value;
+        let d_term = self.d_gain * (self.prev_error - error) / delta_t;
+        self.prev_error = error;
 
         p_term + d_term + i_term
-    }
-}
-
-struct PIDDriver {
-    pub pid: PIDController,
-    pub target: f64,
-}
-
-impl PIDDriver {
-    fn new(pid: PIDController, target: f64) -> PIDDriver {
-        PIDDriver {
-            pid: pid,
-            target: target,
-        }
-    }
-
-    fn input(&mut self, value: f64) -> f64 {
-        let error = self.target - value;
-        self.pid.update(error, value)
     }
 }
