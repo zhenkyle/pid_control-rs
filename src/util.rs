206228ab0c5630bfd796c43c5e528794ad36807e
Marc Brinkmann <git@marcbrinkmann.de>
2016-04-09 18:44:43 +0200

    Docs for utility module.

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,9 @@
+//! Utility module.
+//!
+//! Contains small functions not related to the core functionality, but still
+//! exposed because they might be useful elsewhere.
+
+/// Cap a value inside a certain range.
 #[inline]
 pub fn limit_range<T>(min: T, max: T, value: T) -> T
 where T: PartialOrd {
